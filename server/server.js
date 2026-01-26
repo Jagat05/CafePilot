1b9dc4416170fa340b98c97c6aebbd057fbdddc9
Jagat05 <joshijagaths@gmail.com>
2026-01-26 20:44:31 +0545

    Feat(Server):Server Setup Express,.env,MongoDB(Mongoose)

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from "express";
+import "dotenv/config";
+import ConnectDB from "./database/db.js";
 
 const app = express();
+// const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+ConnectDB();
 
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
